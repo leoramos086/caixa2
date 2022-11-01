ce8e4e755eb98be0e10031fc4bb945110e27ae44
leandro-oliveira <leandro.oliveira@unyleya.edu.br>
2022-11-01 18:45:38 -0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="content">
+        <h1 class="has-text-centered ">VITE + TYPESCRIPT + VUE + BULMA</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+
